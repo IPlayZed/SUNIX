91ac49d6bea5d70fc62c2ea715b6f45ea19e890a
IPlayZed <pauljouser@gmail.com>
2021-08-29 13:48:36 +0200

    Added linker script, made main kernel func linkable, added ignoring of output kernel binary.

--- kernel.cpp
+++ kernel.cpp
@@ -4,6 +4,14 @@ typedef unsigned short USHORT;
 #define VRAM_ADDRS (USHORT*)0xb8000
 #define MESSAGE (char*)"Kernel main test"
 
+/* As we do not use glibc, we must write our very basic implementation of print.
+ * This implementation only support putting ASCII characters on the screen, with black background and white text.
+ * The idea is that adressing VRAM_ADDRS will translate to the GPU hardware putting text on VGA output.
+ * The high byte is the color code, which we leave the default (hence the black-white color of the output) and
+ * the low byte is the ASCII character. By writing on this adress following this idea, we can put text on the screen
+ * in a very basic manner.
+ * 
+ * */
 void printf(char* str) 
 { 
  USHORT* vram= VRAM_ADDRS;
@@ -20,7 +28,7 @@ void printf(char* str)
  * to the stack. The multiboot structure can be used for kernel operation, while
  * the multiboot flag can be used to make sure the enviroment is indeed multiboot.
  * */
-void kernel_main(void* multiboot_struct, unsigned int multiboot_flag) {
+extern "C" void kernel_main(void* multiboot_struct, unsigned int multiboot_flag) {
     printf(MESSAGE);
     while(1);
 }
